c756973e0be97f0693e5f024c1b9cd91a877363b
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-03-06 10:07:58 +0100

    adding env and libraries

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+
+
+
+
+dotenv.config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: (process.env.DB_TYPE as any) || 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: true,
+        autoLoadEntities: true,
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
