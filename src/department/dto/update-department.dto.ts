9cf792a73581878fda38b222108d39d2e3ec02a1
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-03-06 10:56:39 +0100

    Update update-department.dto.ts

--- src/department/dto/update-department.dto.ts
+++ src/department/dto/update-department.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDepartmentDto } from './create-department.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {}
+export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+}
